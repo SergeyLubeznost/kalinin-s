ac136f60aaf6b6ad34a73116855e61c051bd3bfc
SergeyLubeznost <Kutsalov-dg-hub@mail.ru>
2025-02-21 18:17:40 +0300

    update

--- src/pages/productPageFolder/productPage.jsx
+++ src/pages/productPageFolder/productPage.jsx
@@ -1,8 +1,18 @@
 import { useParams } from 'react-router-dom';
 
 import React, { useState } from 'react';
+
+import "./productPage-desktop.css";
+import "./productPage-laptop.css";
+import "./productPage-tablet.css";
+import "./productPage-mobile.css";
+
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
+//import Button from '@mui/material/Button';
+import Checkbox from '@mui/material/Checkbox';
+import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
+import Favorite from '@mui/icons-material/Favorite';
 
 // Import Swiper styles
 import 'swiper/css';
@@ -11,6 +21,7 @@ import 'swiper/css/navigation';
 import 'swiper/css/thumbs';
 
 import './styleSwiperSliderProduct.css';
+import data from '../main/data';
 
 // import required modules
 import { FreeMode, Navigation, Thumbs } from 'swiper/modules';
@@ -18,15 +29,17 @@ import { FreeMode, Navigation, Thumbs } from 'swiper/modules';
 const ProductPage = () => {
     const [thumbsSwiper, setThumbsSwiper] = useState(null);
     const { itemId } = useParams();
+    const product = data.find((item) => item.id === parseInt(itemId));
   
     // Загрузите информацию о товаре с ID itemId
   
     return (
-      <div>
-        {/* Отобразите информацию о товаре */}
+      <main className='main__product__page'>
+        
         <h1>Hello product {itemId}</h1>
-
-        <Swiper
+        <section className="slider__select__color__container">
+          <div>
+          <Swiper
         style={{
           '--swiper-navigation-color': '#fff',
           '--swiper-pagination-color': '#fff',
@@ -110,7 +123,20 @@ const ProductPage = () => {
           <img src="https://swiperjs.com/demos/images/nature-10.jpg" alt="Описание изображения"/>
         </SwiperSlide>
       </Swiper>
-      </div>
+          </div>
+          <div>
+            <div className="select__color__container">
+              <div className="select__color__header">
+              <h2>{product.title}</h2>
+              <p className="select__color__header__likes"> <Checkbox className="image-like" icon={<FavoriteBorder sx={{ color: '#EA899A' }} />} checkedIcon={<Favorite sx={{ color: '#EA899A' }} />} /> {product.likes}</p>
+              </div>
+              <p className="">Материал: {product.material}</p>
+              <p>{product.price} р.</p>
+            </div>
+          </div>
+        </section>
+       
+      </main>
     );
   };
 
