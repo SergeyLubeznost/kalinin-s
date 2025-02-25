88aaaa77c5ec73e683eee77f0630b3da3fcb278a
SergeyLubeznost <Kutsalov-dg-hub@mail.ru>
2025-02-25 22:14:22 +0300

    update

--- src/pages/productPageFolder/productPage.jsx
+++ src/pages/productPageFolder/productPage.jsx
@@ -1,6 +1,7 @@
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
 
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 import "./productPage-desktop.css";
 import "./productPage-laptop.css";
@@ -8,238 +9,249 @@ import "./productPage-tablet.css";
 import "./productPage-mobile.css";
 
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 //import Button from '@mui/material/Button';
-import Checkbox from '@mui/material/Checkbox';
-import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
-import Favorite from '@mui/icons-material/Favorite';
+import { styled } from '@mui/material/styles';
+import Checkbox from "@mui/material/Checkbox";
+import FavoriteBorder from "@mui/icons-material/FavoriteBorder";
+import Favorite from "@mui/icons-material/Favorite";
+
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+
+import Button from "@mui/material/Button";
+
+import Breadcrumbs from "@mui/material/Breadcrumbs";
+import Typography from "@mui/material/Typography";
+
+import Stack from "@mui/material/Stack";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import InputBase from '@mui/material/InputBase';
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/free-mode';
-import 'swiper/css/navigation';
-import 'swiper/css/thumbs';
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import "swiper/css/thumbs";
 
-import './styleSwiperSliderProduct.css';
-import data from '../main/data';
+import "./stylesSwiperProduct/styleSwiperSliderProduct.css";
+import data from "../main/data";
+
+import iconComplect from "./imagePageProduct/iconComplect.svg"
 
 // import required modules
-import { FreeMode, Navigation, Thumbs } from 'swiper/modules';
+import { FreeMode, Navigation, Thumbs } from "swiper/modules";
 
 const ProductPage = () => {
-    const [thumbsSwiper, setThumbsSwiper] = useState(null);
-    const { itemId } = useParams();
-    const product = data.find((item) => item.id === parseInt(itemId));
+  const [thumbsSwiper, setThumbsSwiper] = useState(null);
+  const { itemId } = useParams();
+  const product = data.find((item) => item.id === parseInt(itemId));
 
-    const [selectedColor, setSelectedColor] = useState(Object.keys(product.color[0])[0]); 
-    const handleColorClick = (color) => {
-      setSelectedColor(color);
-    };
-  
-    // Загрузите информацию о товаре с ID itemId
-  
-    return (
-      <main className="main__product__page">
-        <h1>Hello product {itemId}</h1>
-        <section className="slider__select__color__container">
-          <div className="select__color__semi__container">
-            <Swiper
-              style={{
-                "--swiper-navigation-color": "#fff",
-                "--swiper-pagination-color": "#fff",
-              }}
-              loop={true}
-              spaceBetween={10}
-              navigation={true}
-              thumbs={{ swiper: thumbsSwiper }}
-              modules={[FreeMode, Navigation, Thumbs]}
-              className="mySwiper3"
-            >
-              <SwiperSlide className="swiper-slide3">
-                <img
-                  src="https://swiperjs.com/demos/images/nature-1.jpg"
-                  alt="Описание изображения"
-                />
-              </SwiperSlide>
-              <SwiperSlide className="swiper-slide3">
-                <img
-                  src="https://swiperjs.com/demos/images/nature-2.jpg"
-                  alt="Описание изображения"
-                />
-              </SwiperSlide>
-              <SwiperSlide className="swiper-slide3">
-                <img
-                  src="https://swiperjs.com/demos/images/nature-3.jpg"
-                  alt="Описание изображения"
-                />
-              </SwiperSlide>
-              <SwiperSlide className="swiper-slide3">
-                <img
-                  src="https://swiperjs.com/demos/images/nature-4.jpg"
-                  alt="Описание изображения"
-                />
-              </SwiperSlide>
-              <SwiperSlide className="swiper-slide3">
-                <img
-                  src="https://swiperjs.com/demos/images/nature-5.jpg"
-                  alt="Описание изображения"
-                />
-              </SwiperSlide>
-              <SwiperSlide className="swiper-slide3">
-                <img
-                  src="https://swiperjs.com/demos/images/nature-6.jpg"
-                  alt="Описание изображения"
-                />
-              </SwiperSlide>
-              <SwiperSlide className="swiper-slide3">
-                <img
-                  src="https://swiperjs.com/demos/images/nature-7.jpg"
-                  alt="Описание изображения"
-                />
-              </SwiperSlide>
-              <SwiperSlide className="swiper-slide3">
-                <img
-                  src="https://swiperjs.com/demos/images/nature-8.jpg"
-                  alt="Описание изображения"
-                />
-              </SwiperSlide>
-              <SwiperSlide className="swiper-slide3">
-                <img
-                  src="https://swiperjs.com/demos/images/nature-9.jpg"
-                  alt="Описание изображения"
-                />
-              </SwiperSlide>
-              <SwiperSlide className="swiper-slide3">
-                <img
-                  src="https://swiperjs.com/demos/images/nature-10.jpg"
-                  alt="Описание изображения"
-                />
-              </SwiperSlide>
-            </Swiper>
-            <Swiper
-              onSwiper={setThumbsSwiper}
-              loop={true}
-              spaceBetween={10}
-              slidesPerView={4}
-              freeMode={true}
-              watchSlidesProgress={true}
-              modules={[FreeMode, Navigation, Thumbs]}
-              className="mySwiper4"
-            >
-              <SwiperSlide className="swiper-slide4">
-                <img
-                  src="https://swiperjs.com/demos/images/nature-1.jpg"
-                  alt="Описание изображения"
-                />
-              </SwiperSlide>
-              <SwiperSlide className="swiper-slide4">
-                <img
-                  src="https://swiperjs.com/demos/images/nature-2.jpg"
-                  alt="Описание изображения"
-                />
-              </SwiperSlide>
-              <SwiperSlide className="swiper-slide4">
-                <img
-                  src="https://swiperjs.com/demos/images/nature-3.jpg"
-                  alt="Описание изображения"
-                />
-              </SwiperSlide>
-              <SwiperSlide className="swiper-slide4">
-                <img
-                  src="https://swiperjs.com/demos/images/nature-4.jpg"
-                  alt="Описание изображения"
-                />
-              </SwiperSlide>
-              <SwiperSlide className="swiper-slide4">
-                <img
-                  src="https://swiperjs.com/demos/images/nature-5.jpg"
-                  alt="Описание изображения"
-                />
-              </SwiperSlide>
-              <SwiperSlide className="swiper-slide4">
-                <img
-                  src="https://swiperjs.com/demos/images/nature-6.jpg"
-                  alt="Описание изображения"
-                />
-              </SwiperSlide>
-              <SwiperSlide className="swiper-slide4">
-                <img
-                  src="https://swiperjs.com/demos/images/nature-7.jpg"
-                  alt="Описание изображения"
-                />
-              </SwiperSlide>
-              <SwiperSlide className="swiper-slide4">
-                <img
-                  src="https://swiperjs.com/demos/images/nature-8.jpg"
-                  alt="Описание изображения"
-                />
-              </SwiperSlide>
-              <SwiperSlide className="swiper-slide4">
-                <img
-                  src="https://swiperjs.com/demos/images/nature-9.jpg"
-                  alt="Описание изображения"
-                />
-              </SwiperSlide>
-              <SwiperSlide className="swiper-slide4">
-                <img
-                  src="https://swiperjs.com/demos/images/nature-10.jpg"
-                  alt="Описание изображения"
-                />
-              </SwiperSlide>
-            </Swiper>
-          </div>
-          <div className="select__color__semi__container">
-            <div className="select__color__container">
-              <div className="select__color__header">
-                <h2>{product.title}</h2>
-                <p className="select__color__header__likes">
-                  {" "}
-                  <Checkbox
-                    className="image-like"
-                    icon={<FavoriteBorder sx={{ color: "#EA899A" }} />}
-                    checkedIcon={<Favorite sx={{ color: "#EA899A" }} />}
-                  />{" "}
-                  {product.likes}
-                </p>
-              </div>
-              <p className="">Материал: {product.material}</p>
-              <div className="select__color__block">
-                <p>
-                  <span>Цвет:</span>{" "}
-                  {selectedColor}
-                </p>
-                <div style={{ display: "flex" }}>
-                  {product.color.map((colorObj) =>
-                    Object.entries(colorObj).map(([key, value]) => (
-                      <div
-                        key={value}
-                        style={{
-                          width: "35px",
-                          height: "35px",
-                          backgroundColor: value,
-                          margin: "5px",
-                          marginRight: "40px",
-                          borderRadius: "50%",
-                          cursor: "pointer",
-                          border:
-                            selectedColor === key
-                              ? "2px solid rgba(89, 89, 89, 0.39)"
-                              : "none",
-                              boxShadow: selectedColor === key ? "0 3px 19px 7px rgba(89, 89, 89, 0.39)" : "none",
-                        }}
-                        onClick={() => handleColorClick(key)}
-                      />
-                    ))
-                  )}
-                </div>
-              </div>
-
-              <p>{product.price} р.</p>
-            </div>
-          </div>
-        </section>
-      </main>
-    );
+  const [selectedColor, setSelectedColor] = useState(
+    Object.keys(product.color[0])[0]
+  );
+  const handleColorClick = (color) => {
+    setSelectedColor(color);
   };
 
-  export default ProductPage
+  const [age, setAge] = React.useState("");
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+  // Загрузите информацию о товаре с ID itemId
+
+  const breadcrumbs = [
+    <Link to="/katalog" className="bread__crumb__link ">
+      Каталог
+    </Link>,
+    <Link to="" className="bread__crumb__link ">
+      {product.category}
+    </Link>,
+    <Typography
+      key="3"
+      className="font__bread__crumb"
+      sx={{ color: "text.primary" }}
+    >
+      {product.title}
+    </Typography>,
+  ];
+
+  const BootstrapInput = styled(InputBase)(({ theme }) => ({
+    'label + &': {
+      marginTop: theme.spacing(2),
+    },
+    '& .MuiInputBase-input': {
+      borderRadius: 4,
+      position: 'relative',
+      backgroundColor: theme.palette.background.paper,
+      border: 'none', // Убираем границу,
+      fontSize: 22,
+      padding: '10px 0px 10px 0px',
+      transition: theme.transitions.create(['border-color', 'box-shadow']),
+      // Use the system font instead of the default Roboto font.
+      fontFamily: [
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+      ].join(','),
+      '&:focus': {
+        borderRadius: 4,
+        borderColor: 'transparent',
+        border: 'none', // Убираем границу при фокусе
+      boxShadow: 'none', // Убираем тень
+      },
+      '&:hover': {
+        border: 'none', // Убираем границу при наведении
+      },
+    },
+  }));
+
+
+  return (
+    <main className="main__product__page">
+      <Stack spacing={2} className="bread__crumbs">
+        <Breadcrumbs
+          separator={<NavigateNextIcon fontSize="small" />}
+          aria-label="breadcrumb"
+        >
+          {breadcrumbs}
+        </Breadcrumbs>
+      </Stack>
+
+      <section className="slider__select__color__container">
+        <div className="select__color__semi__container rotate__container">
+          <Swiper
+            style={{
+              "--swiper-navigation-color": "#fff",
+              "--swiper-pagination-color": "#fff",
+            }}
+            loop={true}
+            spaceBetween={10}
+            navigation={true}
+            thumbs={{ swiper: thumbsSwiper }}
+            modules={[FreeMode, Navigation, Thumbs]}
+            className="mySwiper3"
+          >
+            {product.imageForCard.map((image, index) => (
+              <SwiperSlide key={index} className="swiper-slide3">
+                <img src={image} alt="Описание изображения" />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+          <Swiper
+            onSwiper={setThumbsSwiper}
+            loop={true}
+            spaceBetween={10}
+            slidesPerView={4}
+            freeMode={true}
+            watchSlidesProgress={true}
+            modules={[FreeMode, Navigation, Thumbs]}
+            className="mySwiper4"
+          >
+            {product.imageForCard.map((image, index) => (
+              <SwiperSlide key={index} className="swiper-slide4">
+                <img src={image} alt="Описание изображения" />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+        <div className="select__color__semi__container">
+          <div className="select__color__container">
+            <div className="select__color__header">
+              <h2>{product.title}</h2>
+              <p className="select__color__header__likes">
+                {" "}
+                <Checkbox
+                  className="image-like"
+                  icon={<FavoriteBorder sx={{ color: "#EA899A" }} />}
+                  checkedIcon={<Favorite sx={{ color: "#EA899A" }} />}
+                />{" "}
+                {product.likes}
+              </p>
+            </div>
+            <p className="select__color__material"><span>Материал:</span> {product.material}</p>
+            <div className="select__color__block">
+              <p>
+                <span>Цвет:</span> {selectedColor}
+              </p>
+              <div style={{ display: "flex", marginBottom: "35px" }}>
+                {product.color.map((colorObj) =>
+                  Object.entries(colorObj).map(([key, value]) => (
+                    <div
+                      key={value}
+                      style={{
+                        width: "35px",
+                        height: "35px",
+                        backgroundColor: value,
+                        margin: "5px",
+                        marginRight: "30px",
+                        borderRadius: "50%",
+                        cursor: "pointer",
+                        border:
+                          selectedColor === key
+                            ? "2px solid rgba(89, 89, 89, 0.39)"
+                            : "2px solid rgba(89, 89, 89, 0.39)",
+                        boxShadow:
+                          selectedColor === key
+                            ? "0 3px 19px 7px rgba(89, 89, 89, 0.39)"
+                            : "none",
+                      }}
+                      onClick={() => handleColorClick(key)}
+                    />
+                  ))
+                )}
+              </div>
+            </div>
+
+            <Box sx={{m: 1, minWidth: 120 }}>
+              <FormControl fullWidth>
+                <InputLabel
+                  id="demo-simple-select-label"
+                  className="textSelect"
+                >
+                  Выбор комплектации <img src={iconComplect} alt="иконка" />
+                </InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={age}
+                  label="Выбор комплектации"
+                  input={<BootstrapInput />}
+                  IconComponent={() => null} // Убираем иконку
+                  onChange={handleChange}
+                >
+                  <MenuItem value={10}>Ten</MenuItem>
+                  <MenuItem value={20}>Twenty</MenuItem>
+                  <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+              </FormControl>
+            </Box>
+
+            <div className="priceProduct__containe">
+              <p>{product.price} р.</p>
+              <Button
+                className="product__button "
+                variant="contained"
+              >
+               Заказать
+              </Button>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default ProductPage;
